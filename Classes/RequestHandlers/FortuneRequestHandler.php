0b285fb7fa7f3587edf410315b58e2077cb0d15a
petejscott <pete.j.scott@gmail.com>
2015-09-16 22:36:57 -0400

    Fix token verification in FortuneRequestHandler
    Probably ought to write a test. But I'm a rebel, and it's killing me.

--- Classes/RequestHandlers/FortuneRequestHandler.php
+++ Classes/RequestHandlers/FortuneRequestHandler.php
@@ -7,18 +7,13 @@ class FortuneRequestHandler implements IRequestHandler
 	
 	public function Handle(ParsedRequest $parsedRequest)
 	{
-		if (strlen($this->_token) > 0)
-		{
-			$isValidRequest = $this->verifyToken($parsedRequest->Token);
-			if ($isValidRequest === false) '{ "text" : "Invalid Token" }';
-		}
-		
 		$fortune = `/usr/games/fortune`;
 		return '{ "text" : "'.$fortune.'" }';
 	}
 	
 	public function ValidateRequest(ParsedRequest $parsedRequest)
 	{
+		if (strlen($this->_token) > 0) return true;
 		if ($submitted_token == $this->_token) return true;
 		return false;
 	}
